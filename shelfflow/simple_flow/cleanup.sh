a8922188a606eb9256435875a6ec665b36f6af11
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-09-14 13:30:50 -0400

    update cleanup script

--- shelfflow/simple_flow/cleanup.sh
+++ shelfflow/simple_flow/cleanup.sh
@@ -2,3 +2,23 @@
 
 # Remove the graph quads.
 wire graph remove -p cleanup/quadparams/quadparams.json
+
+# Remove the items from mongo.
+for line in $(cat "cleanup/items/items.txt"); do
+	sponge item delete -i "$line"
+done
+
+# Remove the patterns from mongo.
+for line in $(cat "cleanup/patterns/patterns.txt"); do
+	xenia pattern delete -t "$line"
+done
+
+# Remove the relationships from mongo.
+for line in $(cat "cleanup/relationships/relationships.txt"); do
+	xenia relationship delete -p "$line"
+done
+
+# Remove the views from mongo.
+for line in $(cat "cleanup/views/views.txt"); do
+	xenia view delete -n "$line"
+done
