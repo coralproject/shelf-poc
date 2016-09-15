b4977227f5bea5ad73622d776903508f9ab07b74
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-09-15 14:34:11 -0400

    update flow script based on new sponged integration

--- shelfflow/simple_flow/runflow.sh
+++ shelfflow/simple_flow/runflow.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Import the items.
-sponge item upsert -p input/items/
-
 # Import the metadata required to generate a view.
 xenia pattern upsert -p metadata/patterns/
 xenia relationship upsert -p metadata/relationships/
 xenia view upsert -p metadata/views/
 
-# Build the graph.
-wire graph add -p input/items/
+# Import the items.
+sponge item upsert -p input/items/
+
+
